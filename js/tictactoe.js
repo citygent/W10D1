65545284da0a5ef0ef90f28e2d00d14e6334fd24
James White <cyberyt@gmail.com>
2015-10-12 19:14:50 +0100

    clicks come thru with values of box

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,6 +1,21 @@
 angular.module('TicTacToe').controller('TicTacToeController', TicTacToeController);
 
 function TicTacToeController() {
-  
+
+this.playCount = 1;
+
+this.play = play;
+
+function play(click){
+  // validation?
+  if (this.playCount %2 === 0) {
+    console.log('even click' + click)
+    this.playCount++;
+
+  } else {
+    console.log('odd click' + click)
+    this.playCount++;
+  }
+};
 
 }
